4cf00b98759a2cecc716dee9b7c57eba444a2db2
EC2 Default User <se.eguti0824@gmail.com>
2022-11-28 07:49:33 +0000

    [add file]dead lock

---
+++ day8/34_dead_lock.sql
@@ -0,0 +1,53 @@
+use day_4_9_db;
+
+start transaction;
+show tables;
+
+select * from customers;
+#主キーでupdate(行ロック)
+update customers set age = 43 where id = 1;
+rollback;
+
+start transaction;
+update customers set age = 42 where name = "河野文典"
+
+#delete 
+start transaction;
+#行ロック
+delete from customers where id = 1;
+commit;
+
+#insert 
+start transaction ;
+insert into customers values(2300,"田中太郎",21,"2000-08-24");
+select *from customers;
+
+#select のロック
+#for shere(共有ロック)
+#for update(排他ロック)
+start transaction;
+select*from customers where id = 1 for share;
+rollback;
+
+start transaction;
+select * from customers where id =1 for update;
+
+#lock table read 
+lock table customers read;
+select * from customers;
+update customers set age = 42 where id = 1;
+
+unlock tables;
+
+#lock table write
+lock table customers write;
+select * from customers;
+update customers set age = 42 where id = 1;
+
+#dead lock
+start transaction
+
+#customers > users
+update customers set age = 42 where id = 1;
+update users set age = 12 where id = 1;
+
